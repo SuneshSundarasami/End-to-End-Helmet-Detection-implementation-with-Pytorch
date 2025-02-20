45f4c19e481f337b57b190f28a205fb34e3cda40
SuneshSundarasami <sunesh@outlook.de>
2025-02-20 11:42:02 +0100

    Data Ingestion from S3 added

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
+from Helmet_Detection_PyTorch.pipeline.train_pipeline import TrainPipeline
 from Helmet_Detection_PyTorch.logger import logging
-from Helmet_Detection_PyTorch.exception import HelmetException
-import sys
 
-
-logging.info("First Log!")
-
-try :
-    a= 2+ "3"
-    print(a)
-except Exception as e:
-    raise HelmetException(e,sys) from e
+train_pipeline = TrainPipeline()
+train_pipeline.run_pipeline()
+logging.info("successfully completed Data Ingestion!")

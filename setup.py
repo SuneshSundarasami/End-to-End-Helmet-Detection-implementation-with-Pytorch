447677892b1b3357e154ed691ea8eae2df627d50
SuneshSundarasami <sunesh@outlook.de>
2025-02-20 06:37:39 +0100

    Added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet_detection",
+    version="0.0.1",
+    author="SuneshSundarasami",
+    author_email="sunesh@outlook.de",
+    packages=find_packages(),
+    install_requires=[],
+)
